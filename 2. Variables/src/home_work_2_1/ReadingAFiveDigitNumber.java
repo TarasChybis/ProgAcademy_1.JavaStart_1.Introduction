1cfc4ea66cccc0f7a38ad45e895b1c73b370d721
Taras Chybis <t0660804676@gmail.com>
2022-08-14 20:38:54 +0300

    English translation

--- 2. Variables/src/home_work_2_1/ReadingAFiveDigitNumber.java
+++ 2. Variables/src/home_work_2_1/ReadingAFiveDigitNumber.java
@@ -1,11 +1,11 @@
-//1) Написать программу которая считывает 5-и значное число с 
-//клавиатуры и выводит цифры из которого оно состоит. 
-//Например : Считывается число 54698
-//Выводиться:	5
-//		4
-//		6
-// 		9
-// 		8
+//1) Write a program that reads a 5 digit number from
+//keyboard and displays the digits from which it consists. 
+//For example: The number is read 54698
+//Output (displays):5
+//            		4
+//            		6
+//            		9
+//            		8
 
 package home_work_2_1;
 
@@ -16,7 +16,7 @@ public class ReadingAFiveDigitNumber {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Введите любое число (например: 54698):");
+		System.out.println("Enter any number (for example: 54698):");
 		int number = sc.nextInt();
 
 		int digit1 = number / 10000;
@@ -25,10 +25,10 @@ public class ReadingAFiveDigitNumber {
 		int digit4 = number % 100 / 10;
 		int digit5 = number % 10;
 
-		System.out.println("Число 1 - " + digit1);
-		System.out.println("Число 2 - " + digit2);
-		System.out.println("Число 3 - " + digit3);
-		System.out.println("Число 4 - " + digit4);
-		System.out.println("Число 5 - " + digit5);
+		System.out.println("Digit 1 - " + digit1);
+		System.out.println("Digit 2 - " + digit2);
+		System.out.println("Digit 3 - " + digit3);
+		System.out.println("Digit 4 - " + digit4);
+		System.out.println("Digit 5 - " + digit5);
 	}
 }
