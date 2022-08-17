1a2c0c169963558a4370a9492bf08cf2b4212625
Taras Chybis <t0660804676@gmail.com>
2022-08-17 22:44:58 +0300

    Cryptotext was decoded from Windows-1251 to UTF-8

--- 3. Operators/src/home_work_3_6/PointInsideTriangle.java
+++ 3. Operators/src/home_work_3_6/PointInsideTriangle.java
@@ -1,3 +1,5 @@
+//A triangle is given with the coordinates of the vertices A(0,0), B(4,4), C(6,1). The user enters x and y coordinates from the keyboard. Write a program that will determine whether this point lies inside the triangle or not.
+
 package home_work_3_6;
 
 import java.util.Scanner;
@@ -24,15 +26,13 @@ public class PointInsideTriangle {
 		yO = sc.nextDouble();
 		
 		if((xA - xO) * (yB - yA) - (xB - xA) * (yA - yO) > 0 && (xB - xO) * (yC - yB) - (xC - xB) * (yB - yO) > 0 && (xC - xO) * (yA - yC) - (xA - xC) * (yC - yO) > 0) {
-			System.out.println("Точка лежит внутри треугольника");
+			System.out.println("The point lies inside the triangle");
 		} else if((xA - xO) * (yB - yA) - (xB - xA) * (yA - yO) < 0 && (xB - xO) * (yC - yB) - (xC - xB) * (yB - yO) < 0 && (xC - xO) * (yA - yC) - (xA - xC) * (yC - yO) < 0) {
-			System.out.println("Точка лежит внутри треугольника");
+			System.out.println("The point lies inside the triangle");
 		} else if((xA - xO) * (yB - yA) - (xB - xA) * (yA - yO) == 0 && (xB - xO) * (yC - yB) - (xC - xB) * (yB - yO) == 0 && (xC - xO) * (yA - yC) - (xA - xC) * (yC - yO) == 0) {
-			System.out.println("Точка лежит на стороне треугольника");	
+			System.out.println("The point lies on the side of the triangle");	
 		} else {
-			System.out.println("Точка лежит вне треугольника");
-		}
-						
+			System.out.println("The point lies outside the triangle");
+		}						
 	}
-
 }
