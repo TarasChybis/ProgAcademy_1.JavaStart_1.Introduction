3d287ddeb74a5d5dd22b679ceba89731cbf798b9
Taras Chybis <t0660804676@gmail.com>
2022-08-19 01:16:54 +0300

    Cryptotext was decoded from Windows-1251 to UTF-8

--- 5. Arrays/src/lesson_5_02/Arrays_2.java
+++ 5. Arrays/src/lesson_5_02/Arrays_2.java
@@ -1,18 +1,18 @@
+//Two-dimensional array
+
 package lesson_5_02;
 
 public class Arrays_2 {
 
 	public static void main(String[] args) {
 
-		int[][] b = new int[4][5];                     // Объявление
-		for (int i = 0; i < b.length; i++) {           // Цикл по строкам
-			for (int j = 0; j < b[0].length; j++) {    // Цикл по столбцам
-				b[i][j] = i + j;                       // Заполнение значениями
-				System.out.print(b[i][j] + " ");       // Вывод на экран
+		int[][] b = new int[4][5];                     // Ad
+		for (int i = 0; i < b.length; i++) {           // Loop through rows
+			for (int j = 0; j < b[0].length; j++) {    // Loop through columns
+				b[i][j] = i + j;                       // Populating with values
+				System.out.print(b[i][j] + " ");       // Output on display
 			}
 			System.out.println();
 		}
-
 	}
-
 }
