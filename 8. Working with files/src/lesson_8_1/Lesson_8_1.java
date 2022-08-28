f2d587222c1fc8695951a69258cd8ec9d443aa40
Taras Chybis <t0660804676@gmail.com>
2022-08-28 18:46:32 +0300

    In the Lesson_8_1.java cryptotext was decoded from Windows-1251 to UTF-8

--- 8. Working with files/src/lesson_8_1/Lesson_8_1.java
+++ 8. Working with files/src/lesson_8_1/Lesson_8_1.java
@@ -11,12 +11,12 @@ public class Lesson_8_1 {
 
 	public static void main(String[] args) {
 		
-//		������ �������� �������� ������ ����������:
+//		Address of the working directory of our application:
 		File workFolder = new File("."); 
 		
-//		������� ������ �������� ������� ����:
+//		Create the object of the file system FILE:
 		File file1 = new File("a.txt");
-		try {                                 //�������� ������ try/catch - ����������� ����������.
+		try {                                 //surround with block try/catch - CHECKED EXCEPTION.
 			file1.createNewFile();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -26,7 +26,7 @@ public class Lesson_8_1 {
 		File folder1 = new File("AAAA");
 	    folder1.mkdirs();
 	    
-//	    ������������� ��������� ������������ ������ ������ �������:
+//	    Relative addressing relative to any other address:
 	    File file2 = new File(folder1, "b.docx");
 	    try {
 	    	file2.createNewFile();
@@ -34,10 +34,10 @@ public class Lesson_8_1 {
 	    	e.printStackTrace();
 	    }
 		
-//	    �������� ������ � �����:
-	    file1.delete();
+//	    Deleting files and folders:
+//	    file1.delete();
 	    file2.delete();
-	    folder1.delete();	//��� ����, ��� �� ������� ����� ����� ������� ������� ��� ����� � ���� �����.
+	    folder1.delete();	//In order to delete a FOLDER, you must first delete ALL FILES in that folder.
 	    
 	    File file = new File("hello.txt");
 	    try (PrintWriter pw = new PrintWriter(file)) {
@@ -47,13 +47,13 @@ public class Lesson_8_1 {
 	    }
 
 	    
-//		������ ������ ����� � ������:
+//		Find out a list of folders and files:
 //		File[] files = workFolder.listFiles();
 //		for (int i = 0; i < files.length; i++) {
 //			System.out.println(files[i]);
 //		}
 		
-//		������ ������ ����� + ������ ������ �����:
+//		Find out the list of folders + size of each folder:
 		File[] files = workFolder.listFiles();
 		for (int i = 0; i < files.length; i++) {
 			System.out.println(files[i] + "\t" + files[i].length());
